98d6e06c886568afc772c1730f607c950b2250f3
zeefandino <zefandion03@gmail.com>
2025-03-24 15:16:28 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 sns.set(style='dark')
 from matplotlib.ticker import FuncFormatter  # Impor FuncFormatter
 
